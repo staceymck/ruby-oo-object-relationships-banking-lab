0343d416cf0c860ce24aecf59f4c8d5a4b3edf30
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-22 20:51:08 -0600

    commit tree 94864d081e5f74cb74d4ab3005992831ba3d2904

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :receiver, :sender, :amount
+  
+ 
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if amount > sender.balance || !valid?
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif valid? && status != "complete" 
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete" 
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    else
+      "#{status} transfer cannot be reversed."
+    end
+  end
+
 end
